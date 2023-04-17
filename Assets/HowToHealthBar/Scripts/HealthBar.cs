66ca5f302b84630dda57e8294336e5899bb40fa1
jialechen3 <jiale1810418002@gmail.com>
2023-04-17 19:54:20 -0400

    Revert "Merge pull request #15 from jialechen3/main"
    This reverts commit 31a316b58d429a4467a44f4670353289044bb0d7, reversing
    changes made to 0d5188455728b71c331fab7b2461393604b415a1.

--- Assets/HowToHealthBar/Scripts/HealthBar.cs
+++ Assets/HowToHealthBar/Scripts/HealthBar.cs
@@ -2,68 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using CodeMonkey.Utils;
-using UnityEngine.SceneManagement;
 
-public class HealthBar : MonoBehaviour
-{
+public class HealthBar : MonoBehaviour {
 
     private Transform bar;
     [SerializeField] private float health = 1f;
     [SerializeField] private float Decreaserate = 0.01f;
     [SerializeField] private float IncreaseRate = 0.3f;
 
-    private void Awake()
-    {
+	private void Awake () {
         bar = transform.Find("Bar");
-    }
+	}
 
-    private void Start()
-    {
-
+    private void Start () {
+        
         FunctionPeriodic.Create(() => {
-            if (health > .01f)
-            {
+            if (health > .01f) {
                 health -= Decreaserate;
                 SetSize(health);
 
-                if (health < .3f)
-                {
+                if (health < .3f) {
                     // Under 30% health
-                    if ((int)(health * 100f) % 3 == 0)
-                    {
+                    if ((int)(health * 100f) % 3 == 0) {
                         SetColor(Color.white);
-                    }
-                    else
-                    {
+                    } else {
                         SetColor(Color.red);
                     }
                 }
-            }
-            else if(health < .01f)
-            {
-                SceneManager.LoadScene("Scene_3");
-            }
-            else
-            {
+            } else {
                 health = 1f;
                 SetColor(Color.red);
             }
         }, .05f);
-    }
+	}
 
 
-    public void SetSize(float sizeNormalized)
-    {
+    public void SetSize(float sizeNormalized) {
         health = sizeNormalized;
         bar.localScale = new Vector3(sizeNormalized, 1f);
     }
 
-    public void Increase()
-    {
+    public void Increase() {
         SetSize(health += IncreaseRate);
     }
-    public void SetColor(Color color)
-    {
+    public void SetColor(Color color) {
         bar.Find("BarSprite").GetComponent<SpriteRenderer>().color = color;
     }
 }
