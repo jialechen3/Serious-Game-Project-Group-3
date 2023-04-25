60d29532c00c63ff44502e1a1282e2e485aa05dd
Fredy Diaz <fredy.diaz@stu.bmcc.cuny.edu>
2023-04-25 13:13:56 -0400

    Revert "Revert "Merge branch 'main' into Fredy""
    This reverts commit 5a325a4ad4d71be754a0237809c446b370669499.

--- Assets/LoseMenu.cs
+++ Assets/LoseMenu.cs
@@ -7,7 +7,7 @@ public class LoseMenu : MonoBehaviour
 {
     public void BackToMenu()
     {
-        SceneManager.LoadScene("Scene_1");
+        SceneManager.LoadScene("main_menu_scene");
     }
     public void Quit()
     {
@@ -15,6 +15,6 @@ public class LoseMenu : MonoBehaviour
     }
     public void Restart()
     {
-        SceneManager.LoadScene("Scene_2");
+        SceneManager.LoadScene("Scene_1");
     }
 }
